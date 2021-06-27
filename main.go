765b4039986a4fe66bcb8fecf3d3f91c8415fffb
ParkKyeongHwan <youcantalktome@naver.com>
2021-06-28 00:26:57 +0900

    Move folders and connect blocks with gohtml

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 	"text/template"
 
-	"github.com/ParkKyeongHwan/practiceCoin/blockchain"
+	"github.com/ParkKyeongHwan/practiceCoin/templates/blockchain"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
@@ -15,12 +20,27 @@ type homeData struct {
 }
 
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.gohtml"))
 	data := homeData{"Home", blockchain.GetBlockChain().Blocks}
-	tmpl.Execute(rw, data)
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockChain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+	templates.ExecuteTemplate(rw, "add", nil)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	http.ListenAndServe(port, nil)
 }
